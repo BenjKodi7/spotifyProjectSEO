c61fb1656903e2bf86d75d34e39bff11de1372f1
Benjamin Kodi <bkodi@vassar.edu>
2024-06-20 14:42:42 +0000

    first get request

---
+++ project.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import re
+import os
+
+# CLIENT_ID = "ad6e6b1ab2924df7898e57312ca14813"
+# CLIENT_SECRET = "d49229ec920d417a98b068b9cebd0e2b"
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+auth_response = requests.post(AUTH_URL, {
+'grant_type': 'client_credentials',
+'client_id': os.environ.get('SPOTIFY_CLIENT_ID'),
+'client_secret': os.environ.get('SPOTIFY_CLIENT_SECRET'),
+})
+
+auth_response_data = auth_response.json()
+access_token = auth_response_data['access_token']
+# print(access_token)
+headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
+
+BASE_URL = 'https://api.spotify.com/v1/'
+
+melanin = "6P4kIjBGUzlieJBkUFobTL"
+game_changer = "5GxeZ0u1qDX95nZwV055JS"
+
+# # tracks = {"uris": ["spotify:track:" + game_changer, "spotify:track:" + melanin]}
+# # playlist_id = "1kT2DU41AV0cK9JQbEm6wS"
+
+getTrack = requests.get(BASE_URL + "tracks/" + melanin, headers=headers)
+
+# # print(addPlaylist.json())
+
+
+print(getTrack.json())
+# print(auth_response_data)
